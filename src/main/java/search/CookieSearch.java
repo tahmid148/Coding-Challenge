1ade96140d3b37ffcaed36a3b8bf90a0e32e69a8
tahmid <tahmidrahman2812@outlook.com>
2023-01-25 02:19:50 +0000

    fix: implemented grouping system to map cookies to occurrences and to find the highest frequency and print out those cookie IDs -- BASIC FUNCTIONALITY ACHIEVED

--- src/main/java/search/CookieSearch.java
+++ src/main/java/search/CookieSearch.java
@@ -8,8 +8,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalLong;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 import static parser.LogCSVParser.parseLogCSV;
 
 public class CookieSearch {
@@ -23,7 +26,15 @@ public class CookieSearch {
                 .map(LogBean::getCookieID)
                 .toList();
 
+        Map<String, Long> groupCookiesByOccurrences = cookiesAtDate.stream().collect(groupingBy(x -> x, counting()));
 
+        OptionalLong highestFrequency = groupCookiesByOccurrences.values().stream().mapToLong(count -> count).max();
+
+        printOutMostActive(groupCookiesByOccurrences, highestFrequency.getAsLong());
+    }
+
+    private static void printOutMostActive(Map<String, Long> cookiesToOccurrences, Long highestFreq) {
+        cookiesToOccurrences.entrySet().stream().filter(x -> x.getValue().equals(highestFreq)).map(Map.Entry::getKey).forEach(System.out::println);
     }
 
 }
