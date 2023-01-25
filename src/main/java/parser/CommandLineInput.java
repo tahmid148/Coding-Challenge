b16b1fec0a952519ed8d386b44206830958eb7da
tahmid <tahmidrahman2812@outlook.com>
2023-01-25 00:12:18 +0000

    feat: implemented CommandLineInput class to store data on filename and date input

--- src/main/java/parser/CommandLineInput.java
+++ src/main/java/parser/CommandLineInput.java
@@ -1,4 +1,29 @@
 package parser;
 
+import java.time.LocalDate;
+
 public class CommandLineInput {
+    private String fileName;
+    private LocalDate date; // LocalDate is used to accommodate the date formatting style
+
+    public CommandLineInput(String fileName, LocalDate date) {
+        this.fileName = fileName;
+        this.date = date;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
